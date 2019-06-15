957593800b85a7c3dbff566dd4100743e0fcff2f
Juan Lopez <juanl0215@gmail.com>
2019-06-14 22:05:09 -0400

    Added Search functionality

--- js/script.js
+++ js/script.js
@@ -23,59 +23,91 @@ const showPage = (students, page = 1) => {
 };
 // Function that calculates max number of pages needed, creates and adds it to the page
 const appendPageLinks = students => {
+  if (document.querySelector('.pagination')) {
+    document.querySelector('.pagination').remove();
+  }
   const maxPages = Math.ceil(students.length / resultPerPage);
   const page = document.querySelector('.page');
   const paginationDiv = document.createElement('div');
   paginationDiv.className = 'pagination';
   const paginationList = document.createElement('ul');
-
   //   Loop through and assign pages and links
   for (x = 1; x <= maxPages; x++) {
     const paginationItem = document.createElement('li');
     const paginationLink = document.createElement('a');
     paginationLink.textContent = x;
+
     paginationItem.appendChild(paginationLink);
     paginationList.appendChild(paginationItem);
+    x === 1 && (paginationLink.className = 'active');
   }
   paginationDiv.appendChild(paginationList);
   page.appendChild(paginationDiv);
 };
+
 // Call Functions passing in "students" as arguments
 showPage(students);
 appendPageLinks(students);
+
+// Creat a Sorry Text for no matches
+const sorryText = document.createElement('h2');
+sorryText.textContent = 'Sorry there are 0 matches';
+
+const handleInput = e => {
+  // Set Each student to display none
+  students.map(student => {
+    return (student.style.display = 'none');
+  });
+  // Filter through the students which what is being inputted in the text field
+  const filterNames = students.filter(student => {
+    return student.children[0].children[1].textContent.includes(e.target.value);
+  });
+  // If there are 0 students that match a search then a sorry Text appears
+  if (filterNames.length === 0) {
+    document.querySelector('.student-list').appendChild(sorryText);
+    sorryText.style.display = '';
+  } else {
+    sorryText.style.display = 'none';
+  }
+  // Creat Pagination links and display students based on filtered names
+  appendPageLinks(filterNames);
+  showPage(filterNames);
+};
+
+// const handleSubmit = e => {
+//   e.preventDefault();
+//   console.log(e.target[0].value);
+// };
+
+// Create Search Component for students
+const searchComponent = students => {
+  const pageHeader = document.querySelector('.page-header');
+  const searchForm = document.createElement('form');
+  const searchLabel = document.createElement('label');
+  const searchInput = document.createElement('input');
+  const searchButton = document.createElement('button');
+  searchButton.textContent = 'Search';
+  searchInput.type = 'text';
+  searchInput.value = '';
+  searchInput.placeholder = 'Enter a name';
+  searchInput.addEventListener('keyup', handleInput);
+  searchLabel.appendChild(searchInput);
+  searchForm.appendChild(searchLabel);
+  searchForm.appendChild(searchButton);
+  searchForm.className = 'student-search';
+  // searchForm.addEventListener('submit', handleSubmit);
+  pageHeader.appendChild(searchForm);
+};
+searchComponent(); // invoke the function
+
 // Event listener that will remove active class from all links, add active to link clicked and then calls the showPage function on the link clicked
 document.querySelector('.pagination ul').addEventListener('click', e => {
   const links = [...document.querySelectorAll('a')];
+
   links.map(link => link.classList.remove('active'));
 
-  e.target.className = 'active';
-  showPage(students, e.target.textContent);
+  if (e.target.nodeName === 'A') {
+    e.target.className = 'active';
+    showPage(students, e.target.textContent);
+  }
 });
-
-// Will come back one day and implement the filtering of app
-
-// const searchComponent = students => {
-//   const pageHeader = document.querySelector('.page-header');
-//   const searchForm = document.createElement('form');
-//   const searchLabel = document.createElement('label');
-//   const searchInput = document.createElement('input');
-//   const searchButton = document.createElement('button');
-//   searchButton.textContent = 'Search';
-//   searchInput.type = 'text';
-//   searchInput.value = '';
-//   searchInput.placeholder = 'Enter a name';
-//   searchLabel.appendChild(searchInput);
-//   searchForm.appendChild(searchLabel);
-//   searchForm.appendChild(searchButton);
-//   searchForm.className = 'student-search';
-//   pageHeader.appendChild(searchForm);
-// };
-
-// document.querySelector('.student-search').addEventListener('submit', e => {
-//   e.preventDefault();
-//   const studentNames = students.map(student =>
-//     student.children[0].children[1].textContent.toLowerCase()
-//   );
-//   const searchedName = document.querySelector('input').value.toLowerCase();
-//   console.log(studentNames.indexOf(searchedName));
-// });
